d283adc5b7fad332cd0d9d709ff09eb673d02e96
soleilyasmina <mounlasolomon@gmail.com>
2019-11-29 21:20:23 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, uniqueness: true
+
   has_many :members
   has_many :projects, through: :members
 end
