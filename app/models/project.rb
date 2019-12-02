8dc81ae508830fb9dab56120cfc3f21361e8685c
soleilyasmina <mounlasolomon@gmail.com>
2019-12-01 19:11:39 -0500

    adjust dependencies

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
-  has_many :songs
-  has_many :setlists
+  has_many :songs, dependent: :destroy
+  has_many :setlists, dependent: :destroy
 
-  has_many :members
+  has_many :members, dependent: :destroy
   has_many :users, through: :members
 end
