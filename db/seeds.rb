bb0adf70406b168ba174b5ef98cd3144425ab6fe
soleilyasmina <mounlasolomon@gmail.com>
2019-12-01 19:12:44 -0500

    update seed with member

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+billiejoe = User.create({ email: 'billiejoearmstrong@gmail.com', username: 'billiejoe', password: 'greenday', first_name: 'Billie Joe', last_name: 'Armstrong' })
+trecool = User.create({ email: 'trecool@gmail.com', username: 'trecool', password: 'greenday', first_name: 'Tre', last_name: 'Cool' })
+mikedirnt = User.create({ email: 'mikedirnt@gmail.com', username: 'mikedirnt', password: 'greenday', first_name: 'Mike', last_name: 'Dirnt' })
+
 green_day = Project.create({ name: 'Green Day', genre: 'Alternative' })
 
 american_idiot = Setlist.create({ title: 'American Idiot', location: 'Canada', time: '2004-09-20 00:00:00', project_id: 1 })
@@ -18,5 +22,10 @@ american_idiot_tracklist = Song.create([
   { title: 'Whatsername', length: 254, project_id: 1 }
 ])
 
+Member.create([
+  { user_id: 1, project_id: 1, admin: true },
+  { user_id: 2, project_id: 1, admin: false },
+  { user_id: 3, project_id: 1, admin: false },
+])
 green_day.setlists << american_idiot
 american_idiot.songs << american_idiot_tracklist
