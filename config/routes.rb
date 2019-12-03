58f25e8ad76a9ae4d033cf8689c5a64db02ca2a8
soleilyasmina <mounlasolomon@gmail.com>
2019-12-03 11:57:01 -0500

    restructure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   resources :projects do
     resources :members, except: [:show]
+    resources :setlists
+    resources :songs
   end
 
-  resources :setlists do
-    put '/songs/:song_id', to: 'setlists_songs#add_song'
-    delete '/songs/:song_id', to: 'setlists_songs#remove_song'
-  end
-  resources :songs
-
+  put '/setlists/:setlist_id/songs/:song_id', to: 'setlists_songs#add_song'
+  delete '/setlists/:setlist_id/songs/:song_id', to: 'setlists_songs#remove_song'
 
   post '/auth/login', to: 'authentication#login'
   post '/auth/register', to: 'authentication#register'
